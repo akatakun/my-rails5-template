5b5da3c416db0f5fe660547f13c042b9e9d7a44f
akata <akata.onen@gmail.com>
2018-08-07 19:41:37 +0900

    Autoload javascript of webpacker each by controller and action

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def autoload_javascript_path
+    p = "#{controller_path}/#{action_name}.js"
+    pack_file_exist?(p) ? p : 'default.js'
+  end
+
+  def pack_file_exist?(path)
+    open('public/packs/manifest.json') do |f|
+      JSON.load(f)
+    end.key?(path)
+  end
 end
