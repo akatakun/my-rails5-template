618db8ba1a030e55ea23f392c21943136772e5bc
akata <akata.onen@gmail.com>
2018-08-07 19:43:48 +0900

    Don't create helper file

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,10 @@ module MyRails5Template
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators do |g|
+      # Don't create assets and helper files, because turn off assets pipeline.
+      g.assets false
+    end
   end
 end
