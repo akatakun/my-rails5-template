76ee0c00665c6b654e0f80c8854a7452e60cdde4
akata <akata.onen@gmail.com>
2018-12-10 22:26:32 +0900

    [Capistrano] Refactoring. And add settings for Nginx and Puma

--- Gemfile
+++ Gemfile
@@ -30,9 +30,6 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
-# Use Capistrano for deployment
-gem 'capistrano-rails', group: :development
-
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
@@ -50,6 +47,11 @@ gem 'rolify'
 gem 'rails_admin', '~> 1.3'
 
 group :development, :test do
+  gem 'capistrano-rails'
+  gem 'capistrano-rbenv'
+  gem 'capistrano-yarn'
+  gem 'capistrano3-puma'
+
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'pry-byebug'
--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,40 @@ set :repo_url, ENV.fetch('REPO_URL', 'https://github.com/akatakun/my-rails5-temp
 set :branch, ENV.fetch('BRANCH', 'master')
 
 set :application, 'my-rails5-template'
-set :migration_role, :db
 
-append :linked_files, 'config/master.key', 'config/master.key'
-append :linked_dirs,  'vendor/bundle', 'node_modules'
+set :rbenv_ruby, File.read('.ruby-version').strip
+
+set :ssh_options, {
+  keys: ['./etc/ssh/my-rails5-template.pem'],
+  user: 'ec2-user',
+}
+
+linked_filenames = ['config/master.key', '.env', 'config/database.yml']
+linked_dirnames  = ['tmp/pids', 'tmp/sockets', 'log']
+
+namespace :deploy do
+  after 'deploy:check:make_linked_dirs', 'deploy:upload'
+  desc 'Upload untracking files'
+  task :upload do
+    on roles :all do
+      linked_filenames.each do |file|
+        upload!(file, "#{shared_path}/#{file}")
+      end
+    end
+  end
+end
+
+append :linked_files, *linked_filenames
+append :linked_dirs,  *linked_dirnames
+
+# Puma
+set :puma_threads,     [5, 10]
+set :puma_workers,     0
+set :puma_bind,        "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_state,       "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,         "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log,  "#{shared_path}/log/puma.access.log"
+set :puma_error_log,   "#{shared_path}/log/puma.error.log"
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true
