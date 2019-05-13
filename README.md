744060475da9ffeb787e29a8602a6df7a7beebaa
akata <akata.onen@gmail.com>
2019-05-13 18:51:04 +0900

    [OmniAuth]   Change to optional

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # my-rails5-template
+## Preparation
+### .envを設定
+```
+# required
+DATABASE_DATABASE=
+DATABASE_USERNAME=
+DATABASE_PASSWORD=
+DATABASE_HOST=
+DATABASE_PORT=
+
+# optional
+TWITTER_APP_KEY=
+TWITTER_SECRET=
+STEAM_APP_KEY=
+```
+
 ## Development
 ### without Docker
 1. Change config/database.yml host and config/webpacker.yml host
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,11 @@
 require 'omniauth-steam'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  raise Exception, 'TWITTER_APP_KEY env is blank!' if !ENV['TWITTER_APP_KEY']
-  raise Exception, 'TWITTER_SECRET env is blank!' if !ENV['TWITTER_SECRET']
-  provider :twitter, ENV['TWITTER_APP_KEY'], ENV['TWITTER_SECRET']
+  if ENV['TWITTER_APP_KEY'] && ENV['TWITTER_SECRET']
+    provider :twitter, ENV['TWITTER_APP_KEY'], ENV['TWITTER_SECRET']
+  end
 
-  raise Exception, 'STEAM_APP_KEY env is blank!' if !ENV['STEAM_APP_KEY']
-  provider :steam, ENV['STEAM_APP_KEY']
+  if ENV['STEAM_APP_KEY']
+    provider :steam, ENV['STEAM_APP_KEY']
+  end
 end
